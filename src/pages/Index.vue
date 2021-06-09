95ebb79edb7af9a3665b27e903349c3acab992ea
Mathieu Zwygart <mathieu@zwygart.net>
2021-06-09 14:17:18 +0200

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <Layout>
+
+    <h1>Image compression test</h1>
+
+    <h2>Regular img tag</h2>
+    <figure>
+      <img src="images/01.jpg" alt="">
+      <figcaption>To display this image, I use regular img tag.<br>No compression, everything looks good</figcaption>
+    </figure>
+
+    <h2>g-image component</h2>
+    <figure>
+      <g-image src="~/assets/images/01.jpg" quality="100"  alt="" />
+      <figcaption>To display this image, I use g-image.<br>There is a lot of compression, even though I set the quality attribut to 100.</figcaption>
+    </figure>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+
+img {
+  max-width: 100%;
+}
+</style>
